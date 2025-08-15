f87ef1f4539e34870bd24f33dba87e4e793a30ac
KrishCore <krishvadaga4@gmail.com>
2025-08-15 10:28:05 -0500

    8/15/25

---
+++ DS0/src/DS0_CH3.java
@@ -0,0 +1,15 @@
+public class DS0_CH3
+{
+    public static int[] fewest (int cents)
+    {
+        int[] values = {1, 5, 10, 25, 100, 500, 1000, 2000, 5000, 10000};
+        int[] result = new int[values.length];
+
+
+        for (int i = result.length-1; i >= 0; i--) {
+            result[i] = cents/values[i];
+            cents %= values[i];
+        }
+        return result;
+    }
+}
