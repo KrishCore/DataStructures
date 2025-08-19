0dec951a38b3057e4065330d660716708115c0b6
KrishCore <krishvadaga4@gmail.com>
2025-08-19 18:27:20 -0500

    ch7 pt2

--- DS0/src/DS0_CH7_Bank.java
+++ DS0/src/DS0_CH7_Bank.java
@@ -4,21 +4,36 @@ public class DS0_CH7_Bank
 {
     private
     String bankName;
-//    ArrayList<Account> accounts;
+    ArrayList<DS0_CH7_Account> accounts;
 
     public DS0_CH7_Bank(String name)
     {
         this.bankName = name;
-//        accounts = new ArrayList<Account>();
+        accounts = new ArrayList<DS0_CH7_Account>();
     }
-//    public boolean openAccount(long accountNumber, String customerName double startingBalance)
-//    {
-//
-//    }
-//    public double closeAccount(long accountNumber)
-//    {
-//
-//    }
+    public boolean openAccount(long accountNumber, String customerName, double startingBalance)
+    {
+        boolean tf = false;
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts.get(i).getAccountNumber() == accountNumber)
+                tf = true;
+        }
+        if (startingBalance <= 0 || tf)
+            return false;
+        else
+        {
+            accounts.add(new DS0_CH7_Account(accountNumber, customerName, startingBalance));
+            return true;
+        }
+    }
+    public double closeAccount(long accountNumber)
+    {
+        for (int i = accounts.size()-1; i >= 0; i--) {
+            if (accounts.get(i).getAccountNumber() == accountNumber)
+                return accounts.remove(i).getBalance();
+        }
+        return -1;
+    }
     public void setName(String bankName)
     {
         this.bankName = bankName;
@@ -27,12 +42,17 @@ public class DS0_CH7_Bank
     {
         return bankName;
     }
-//    public Account getAccount(long accountNumber)
-//    {
-//
-//    }
-//    public ArrayList<Account> getAccounts()
-//    {
-//        return accounts;
-//    }
+    public DS0_CH7_Account getAccount(long accountNumber)
+    {
+        DS0_CH7_Account ga = null;
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts.get(i).getAccountNumber() == accountNumber)
+                ga = accounts.get(i);
+        }
+        return ga;
+    }
+    public ArrayList<DS0_CH7_Account> getAccounts()
+    {
+        return accounts;
+    }
 }
