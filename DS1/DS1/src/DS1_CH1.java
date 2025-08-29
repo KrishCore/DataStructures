5e97e734e8b74c4d09ffc0a418905bf73e214fe8
KrishCore <krishvadaga4@gmail.com>
2025-08-29 10:11:28 -0500

    Update DS1_CH1.java

--- DS1/DS1/src/DS1_CH1.java
+++ DS1/DS1/src/DS1_CH1.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DS1_CH1
@@ -6,14 +7,19 @@ public class DS1_CH1
     public static int[] sumLines (String fileName)
     {
         File fileRef = new File(fileName);
+        ArrayList <Integer> num = new ArrayList<>();
         try {
             Scanner sc = new Scanner(fileRef);
             int i = 0;
             while (sc.hasNextLine()) {
                 Scanner scan = new Scanner(sc.nextLine()).useDelimiter("[,]");
-                while (scan.hasNextInt())
-                    
+                while (scan.hasNextInt()) {
+                    num.add(scan.nextInt());
                     System.out.print(scan.nextInt() + "  ");
+                }
+            }
+            for (int j = 0; j < num.size(); j++) {
+                System.out.println(num.get(i));
             }
         } catch (Exception e) {
            e.printStackTrace();
