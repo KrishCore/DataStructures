5b50c7ecd9bc710a054ea8f434b26c94bf1acad7
KrishCore <krishvadaga4@gmail.com>
2025-09-05 10:00:11 -0500

    DS1_CH4 needs work

--- DS1/DS1/src/DS1_CH3.java
+++ DS1/DS1/src/DS1_CH3.java
@@ -6,30 +6,20 @@ public class DS1_CH3
 {
     public static int[] rowSums(String fileName)
     {
-        int[] row = new int[0];
-        ArrayList <Integer> nr = new ArrayList<>();
         ArrayList <Integer> num = new ArrayList<>();
-        String[] rc = new String[0];
+
         try {
             File fileRef = new File(fileName);
             Scanner fileSc = new Scanner(fileRef);
-            while (fileSc.hasNextLine()) {
-                // scanner for each specific line
-                String firstLine = fileSc.nextLine();
-                rc = fileSc.next().split("X");
-                nr.add(fileSc.nextInt());
 
-
-//                	Scanner fromFile = new Scanner(new File("Files\\Final_Grades_v1.txt"));
-//
-//			// Reads the first number in the file. This is the number of grade that will follow
-//			int numItems = fromFile.nextInt();
-            }
-            row = new int[(int) (rc[1])];
+            // read each line
             while (fileSc.hasNextLine()) {
                 String line = fileSc.nextLine();
+
+                // scanner for each specific line
+                Scanner lineSc = new Scanner(line).useDelimiter("[ ,]");
+
                 int total = 0;
-                Scanner lineSc = new Scanner(line).useDelimiter(" ");
                 while (lineSc.hasNextInt()) {
                     total += lineSc.nextInt();
                 }
@@ -37,17 +27,63 @@ public class DS1_CH3
                 num.add(total);
                 lineSc.close();
             }
-            for (int i = 0; i < num.size(); i++) {
-                row[i] = num.get(i);
-            }
 
+            fileSc.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        int[] row = new int[num.size()-1];
+        for (int i = 0; i < row.length; i++) {
+            row[i] = num.get(i+1);
+        }
         return row;
     }
-//    public static int[] columnSums (String fileName)
-//    {
-//
-//    }
+
+    public static int[] columnSums (String fileName)
+    {
+        ArrayList <Integer> num = new ArrayList<>();
+
+        int[] col = new int[0];
+        try {
+            File fileRef = new File(fileName);
+            Scanner fileSc = new Scanner(fileRef);
+
+//            4X4
+//            1   2   3   4
+//            2   3   4   5
+//            6   1   3   2
+//            3   4   5   6
+
+            String line = fileSc.nextLine();
+            int[] f = new int[]{Integer.parseInt(line.split("X")[0])};
+            int n = f[0];
+            col = new int[n];
+
+            // read each line
+            while (fileSc.hasNextLine()) {
+                line = fileSc.nextLine();
+                // scanner for each specific line
+                Scanner lineSc = new Scanner(line).useDelimiter("[ ,]");
+
+                int total = 0;
+                int i = 0;
+                while (lineSc.hasNextInt()) {
+                    col[i] += lineSc.nextInt();
+                    i++;
+                    i %= n;
+                }
+
+                num.add(total);
+                lineSc.close();
+            }
+
+            fileSc.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        for (int i = 0; i < col.length; i++) {
+//            col[i] = num.get(i+1);
+//        }
+        return col;
+    }
 }
