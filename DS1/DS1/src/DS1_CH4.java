35ec2cb1497e48765f743ade3f175e90512595cc
KrishCore <krishvadaga4@gmail.com>
2025-09-08 09:42:23 -0500

    DS1_CH4 still needs work

--- DS1/DS1/src/DS1_CH4.java
+++ DS1/DS1/src/DS1_CH4.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class DS1_CH4
@@ -7,15 +8,15 @@ public class DS1_CH4
     public static int largestAreaSum (String fileName)
     {
         ArrayList <Integer> num = new ArrayList<>();
+        int[][] grid1;     // = null; //new int[0][0];
 
         try {
             File fileRef = new File(fileName);
             Scanner fileSc = new Scanner(fileRef);
-            String[] size = fileSc.nextLine().split("X");
-            int c = Integer.parseInt(size[0]);
-            int r = Integer.parseInt(size[1]);
-
-            int[][] grid = new int[r][c];
+//            String[] size = fileSc.nextLine().split("X");
+//            int c = Integer.parseInt(size[0]);
+//            int r = Integer.parseInt(size[1]);
+            String gridS = "";
 
             // read each line
             while (fileSc.hasNextLine()) {
@@ -24,48 +25,61 @@ public class DS1_CH4
                 // scanner for each specific line
                 Scanner lineSc = new Scanner(line).useDelimiter("[ ,]");
 
-                int row = 0;
-                int col = 0;
-                while (lineSc.hasNextInt() && col < c) {
-                    grid[row][col] = lineSc.nextInt();
-                    System.out.println(grid[row][col]);
-                    c++;
+                // adds each in tto a mega string
+                while (lineSc.hasNextInt()) {
+                    gridS += lineSc.nextInt() + " ";
                 }
-                r++;
+                gridS += "\n";
                 lineSc.close();
             }
 
-            for (int i = 0; i < r; i++) {
-                for (int j = 0; j < c; j++) {
-                    if (i == 0)
+            // splits each int value into an array
+            String[] line = gridS.split("\n");
+            String[][] numb = new String[line.length][line[0].length()];
+            int[][] grid = new int[line.length][line[0].length()];
+            for (int i = 0; i < line.length; i++) {
+                numb[i] = line[i].split(" ");
+                System.out.println(Arrays.toString(numb[i]));
+            }
+
+            // copies string values to an int array
+//            int r = grid.length;
+//            int c = grid[0].length;
+            for (int i = 0; i < line.length; i++) {
+                for (int j = 0; j < line[0].length(); j++) {
+                    grid[i][j] = Integer.parseInt(numb[i][j]);
+                }
+            }
+
+            // adds the sides up and puts it in an arraylist
+            for (int i = 0; i < line.length; i++) {
+                for (int j = 0; j < line[0].length(); j++) {
+                    if (i == 0) // if on top
                     {
-                        if (j == 0)
+                        if (j == 0) // if top left
                             num.add(grid[i][j] + grid[i+1][j] + grid[i][j+1]);
-                        else if (j == c-1)
+                        else if (j == line[0].length()-1) // if top right
                             num.add(grid[i][j] + grid[i+1][j] + grid[i][j-1]);
-                    } else if (i == r - 1)
+                    } else if (i == line.length - 1) // if bottom
                     {
-                        if (j == 0)
+                        if (j == 0) // if bottom left
                             num.add(grid[i][j] + grid[i-1][j] + grid[i][j+1]);
-                        else if (j == c-1)
+                        else if (j == line[0].length()-1) // if bottom right
                             num.add(grid[i][j] + grid[i-1][j] + grid[i][j-1]);
-                    }
+                    } // if none of the above (centers)
                     else num.add(grid[i][j] + grid[i-1][j] + grid[i+1][j] + grid[i][j+1] + grid[i][j-1]);
                 }
             }
+            grid1 = grid;
             fileSc.close();
         } catch (Exception e) {
+            System.out.println("Error");
             e.printStackTrace();
         }
-//        finally {
-//            int[] arr = new int[num.size()];
-//            for (int i = 0; i < arr.length; i++) {
-//                arr[i] = num.get(i);
-//            }
-//        }
-        int max = 0;
+        // finds the highest value in the arraylist of sums
+        int max = 0;       //grid1[0][0]; // + grid1[1][0] + grid1[0][1];
         for (int i = 0; i < num.size(); i++) {
-            if (num.get(i) < max)
+            if (max < num.get(i))
                 max = num.get(i);
         }
         return max;
