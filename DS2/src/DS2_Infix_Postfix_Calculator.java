d265f6980c06ebd7b72e9353d9152be02b35c4f5
KrishCore <krishvadaga4@gmail.com>
2025-09-25 10:27:31 -0500

    Update DS2_Infix_Postfix_Calculator.java

--- DS2/src/DS2_Infix_Postfix_Calculator.java
+++ DS2/src/DS2_Infix_Postfix_Calculator.java
@@ -1,12 +1,13 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class DS2_Infix_Postfix_Calculator// <E> implements StackInterface<E>
 {
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter an equation in infix form (separating values and operators with spaces): ");
+        System.out.print("Enter an equation in infix form (separating values and operators with spaces): \n");
         String equation = scan.nextLine();
         String itp = infixToPostfix(equation);
         System.out.println("..equation: " + equation);
@@ -18,61 +19,81 @@ public class DS2_Infix_Postfix_Calculator// <E> implements StackInterface<E>
     {
         String[] list = infix.split(" ");
         ArrayList<Double> me = new ArrayList<>();
-        String modEqu = "";
-        ArrayList <String> ops = new ArrayList<>();
+        Stack<Object> stack = new Stack<>();
+        String postfixOutput = "";
+//        ArrayList <String> ops = new ArrayList<>();
         for (int i = 0; i < list.length; i++) {
-            boolean addOp = false;
-            if (list[i].equals("+")) {
-                ops.add("+");
+//            boolean addOp = false;
+
+            if (list[i].equals("("))
+                stack.push("(");
+
+            else if (list[i].equals(")"))
+                while (stack.pop() != "(") {}
+
+            if (list[i].equals("+") && !stack.isEmpty() && stack.peek() != "(" && (stack.peek() == "*" || stack.peek() == "/" || stack.peek() == "+" || stack.peek() == "-" || stack.peek() == "^")) {
+                stack.push("+");
+//                ops.add("+");
             }
-            else if (list[i].equals("-")) {
-                ops.add("-");
+            else if (list[i].equals("-") && !stack.isEmpty() && stack.peek() != "(" && (stack.peek() == "*" || stack.peek() == "/" || stack.peek() == "+" || stack.peek() == "-" || stack.peek() == "^"))
+            {
+                stack.push("-");
+//                ops.add("-");
             }
-            else if (list[i].equals("*")) {
-                ops.add("*");
-                addOp = true;
+//            + - * / ^
+//            else if (list[i].equals("+") || list[i].equals("-") || list[i].equals("*") || list[i].equals("/") || list[i].equals("^")) {
+////                while (!stack.isEmpty() && stack.peek() != "(" && stack.peek() != "*" && stack.peek() != "/")
+////                    stack
+//            }
+            else if (list[i].equals("*") && !stack.isEmpty() && stack.peek() != "(" && stack.peek() != "^") {
+                stack.push("*");
+//                ops.add("*");
             }
-            else if (list[i].equals("/")) {
-                ops.add("/");
-                addOp = true;
+            else if (list[i].equals("/") && !stack.isEmpty() && stack.peek() != "(" && stack.peek() != "^") {
+                stack.push("/");
+//                ops.add("/");
             }
-            else if (list[i].equals("(")) {
-                continue;
+
+            else if (list[i].equals("^") && !stack.isEmpty() && stack.peek() != "(" || stack.peek()== "^") {
+                stack.push("^");
             }
-            else if (list[i].equals(")")) {
-                continue;
-            }
-            else if (list[i].equals("^")) {
-                addOp = true;
-            }
-            else if (Double.parseDouble(list[i]) >= 0) {
-                me.add(Double.parseDouble(list[i]));
-                modEqu += list[i] + " ";
-            }
-            if (addOp) // need to add after the second nuber is added
-                modEqu += ops.getLast() + " ";
+
+//            else if (Double.parseDouble(list[i]) >= 0) {
+//                me.add(Double.parseDouble(list[i]));
+//                postfixOutput += list[i] + " ";
+//            }
+//            if (addOp) { // need to add after the second nuber is added
+//                postfixOutput += ops.getLast() + " ";
+//                addOp = false;
+//            }
+            System.out.println(stack);
+
         }
-        String temp = modEqu;
-        for (int i = 0; i < me.size(); i++) {
-            double num = me.get(i);
-            char c = (char) num;
-            if (infix.charAt(i) == c){}
-                // needs work
-        }
-        return modEqu;      //me.toString();//.substring(0,me.size());
+
+        String[] temp = postfixOutput.split(" ");
+
+//        for (int i = 0; i < me.size(); i++) {
+//            double num = me.get(i);
+//            char c = (char) num;
+//            if (infix.charAt(i) == c){}
+//                // needs work
+//        }
+        while (!stack.isEmpty())
+            postfixOutput += stack.pop();
+        return stack.toString() + "___" + postfixOutput;      //me.toString();//.substring(0,me.size());
     }
     public static double solvePostfix (String postFix)
     {
-        String[] list = postFix.split(" ");
+//        String[] list = postFix.split(" ");
         double sum = 0;
-        for (int i = 0; i < list.length; i++) {
-            if (list[i].equals("^"))
-                sum += (double) Math.pow(Double.parseDouble(list[i-2]), Double.parseDouble(list[i-1]));
-            if (list[i].equals("*"))
-                sum += (double) Double.parseDouble(list[i-2]) * Double.parseDouble(list[i-1]);
-            if (list[i].equals("/"))
-                sum += (double) Double.parseDouble(list[i-2]) / Double.parseDouble(list[i-1]);
-        }
+//        for (int i = 0; i < list.length; i++) {
+//            if (list[i].equals("^"))
+//                sum += (double) Math.pow(Double.parseDouble(list[i-2]), Double.parseDouble(list[i-1]));
+//            if (list[i].equals("*"))
+//                sum += (double) Double.parseDouble(list[i-2]) * Double.parseDouble(list[i-1]);
+//            if (list[i].equals("/"))
+//                sum += (double) Double.parseDouble(list[i-2]) / Double.parseDouble(list[i-1]);
+//        }
         return sum;
     }
 
