d9da4b930ec39e461a5db3c82c2f602bad6da0e8
KrishCore <krishvadaga4@gmail.com>
2025-10-07 10:25:26 -0500

    Update DS3_UniqueWords.java

--- DS3/src/DS3_UniqueWords.java
+++ DS3/src/DS3_UniqueWords.java
@@ -18,38 +18,23 @@ public class DS3_UniqueWords
             {
                 String line = fs.nextLine().toLowerCase();
                 String[] lin = line.split(" ");
-                Scanner ls = new Scanner(line).useDelimiter(" ");
-                String string = ls.next();
+//                Scanner ls = new Scanner(line).useDelimiter(" ");
+//                String string = ls.next();
 
                 for (int i = 0; i < lin.length; i++) {
 
-
-                    for (int k = 0; k < lin[i].length(); k++) {
-                        if (".,!?;:\"\'-".contains(lin[i].charAt(k)+""))
-                        {
-                            if (k ==0) {
-                                lin[i] = (lin[i].substring(1));
-                            }
-                            else if (k ==lin[i].length()-1) {
-                                lin[i] = (lin[i].substring(0, lin[i].length()-2));
-                            }
-                            else if (k>0 && k <string.length()-1) {
-                                lin[i] = (lin[i].substring(0, k) + lin[i].substring(k +1));
-                            }
-                        }
-//                        else set.add(string);
+                    char[] chars = lin[i].toCharArray();
+                    lin[i] = "";
+                    for (int j = 0; j < chars.length; j++) {
+                        if (!".,!?;:\"\'’-–\n ()".contains(chars[j]+""))
+                            lin[i] += chars[j];
                     }
                 }
-
-                while (ls.hasNext()) {
-                    set.add(string.toLowerCase());
-                    ls.next();
-                }
-
 
                 for (int i = 0; i < lin.length; i++) {
                     set.add(lin[i]);
                 }
+
             }
         }
         catch (Exception e) {
