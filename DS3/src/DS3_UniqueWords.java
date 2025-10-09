56aa3b141e90bcd59d52315eb891509ca53dfc49
KrishCore <krishvadaga4@gmail.com>
2025-10-09 10:25:24 -0500

    Update DS3_UniqueWords.java

--- DS3/src/DS3_UniqueWords.java
+++ DS3/src/DS3_UniqueWords.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Scanner;
 
@@ -21,18 +22,39 @@ public class DS3_UniqueWords
 //                Scanner ls = new Scanner(line).useDelimiter(" ");
 //                String string = ls.next();
 
+                System.out.println("\0");
                 for (int i = 0; i < lin.length; i++) {
 
-                    char[] chars = lin[i].toCharArray();
-                    lin[i] = "";
-                    for (int j = 0; j < chars.length; j++) {
-                        if (!".,!?;:\"\'’-–\n ()".contains(chars[j]+""))
-                            lin[i] += chars[j];
+
+                    if (!lin[i].isEmpty() || !lin[i].isBlank() || !(lin[i].equals(""))) {
+                        char[] chars = lin[i].toCharArray();
+                        System.out.println("_"+ Arrays.toString(chars) +"_");
+//                        error is the blanks are punctuation
+                        // for test10
+                        //_["]_
+                        //truetruetruetrue[] ___
+                        //_[–]_
+                        //truetruetruetrue[] ___
+                        System.out.print(!lin[i].isEmpty() + "" + !lin[i].isBlank() + !(lin[i].equals("")) + (chars.length>0) + "[] ");
+                        lin[i] = "";
+                        if (!".,!?;:\"'’-–\n ()".contains(chars[0]+"")) {
+                            for (int j = 0; j < chars.length; j++) {
+                                System.out.print(Character.isWhitespace(chars[j]) + "--");
+                                if (!".,!?;:\"'’-–\n ()".contains(chars[j] + "") && !Character.isWhitespace(chars[j])) {
+                                    System.out.print((chars[j] == '\0' && "\0".contains(chars[j] + "")) + "  ");
+                                    lin[i] += chars[j];
+                                    System.out.print(chars[j] + "_");
+                                }
+                            }
+                        }
+
+                        set.add(lin[i]);
+                        System.out.println("___"+lin[i]);
                     }
                 }
 
                 for (int i = 0; i < lin.length; i++) {
-                    set.add(lin[i]);
+//                    set.add(lin[i]);
                 }
 
             }
