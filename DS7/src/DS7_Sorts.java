48572dd26db2ccd4591be78f8e05cbac95a0f985
KrishCore <krishvadaga4@gmail.com>
2026-01-07 09:08:30 -0600

    Update DS7_Sorts.java

--- DS7/src/DS7_Sorts.java
+++ DS7/src/DS7_Sorts.java
@@ -1,17 +1,18 @@
 public class DS7_Sorts
 {
+    static int[] tempData = new int[0];
+
     public static void selectionSort(int[] list)
     {
         for (int a = 0; a < list.length; a++) {
             int minIndex = a;
             for (int b = a + 1; b < list.length; b++) {
-                if (list[b] < list[minIndex]) {
+                if (list[b] < list[minIndex])
                     minIndex = b;
-                    int temp = list[minIndex];
-                    list[minIndex] = list[a];
-                    list[a] = temp;
-                }
             }
+            int temp = list[minIndex];
+            list[minIndex] = list[a];
+            list[a] = temp;
         }
     }
 
@@ -28,17 +29,15 @@ public class DS7_Sorts
         }
     }
 
-    static int[] tempData = new int[1];
-
     public static void mergeSort(int[] data, int from, int to)
     {
-        if (tempData == null || tempData.length != data.length)
+        if (tempData == null || data.length != tempData.length)
             tempData = new int[data.length];
 
         if (to == from)
             return;
 
-        int middle = (from+to)/2;
+        int middle = (from+to )/2;
         mergeSort(data, from, middle);
         mergeSort(data, middle+1, to);
 
@@ -46,22 +45,60 @@ public class DS7_Sorts
         int j = middle+1;
         int k = from;
 
-        while (i<=middle && j <= to) {
+        while (i<=middle && j <= to)
+        {
             if (data[i] < data[j])
                 tempData[k++] = data[i++];
             else
                 tempData[k++] = data[j++];
 
-            while (i <= middle)
-                tempData[k++] = data[i++];
+        }
+        while (i <= middle)
+            tempData[k++] = data[i++];
 
-            while (j <= to)
-                tempData[k++] = data[j++];
+        while (j <= to)
+            tempData[k++] = data[j++];
 
-            for (k = from; k <= to; k++)
-            {
-                data[k] = tempData[k];
+        for (int a = from; a < to; a++)
+            data[a] = tempData[a];
+    }
+
+    public static void quickSort(int[] data, int from, int to)
+    {
+        if (from >= to)
+            return;
+
+        int p = (from+to)/2;
+        int i=from;
+        int j=to;
+
+        while (i<=j) {
+            if (data[i] <= data[p])
+                i++;
+            else if (data[j] >= data[p])
+                j--;
+            else {
+                int temp = data[j];
+                data[j] = data[i];
+                data[i] = data[j];
+                i++;
+                j--;
             }
         }
+        if (p < j) {
+            int temp = data[p];
+            data[p] = data[j];
+            data[j] = temp;
+            p = j;
+        }
+        else if (p > i) {
+            int temp = data[p];
+            data[p] = data[i];
+            data[i] = temp;
+            p = i;
+        }
+
+        quickSort(data, from, p-1);
+        quickSort(data, p+1, to);
     }
 }
