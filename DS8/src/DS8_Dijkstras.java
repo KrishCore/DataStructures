200e38188973d024c53bf925bf6c26cc0b9641d2
KrishCore <krishvadaga4@gmail.com>
2026-01-26 19:00:41 -0600

    Update DS8_Dijkstras.java

--- DS8/src/DS8_Dijkstras.java
+++ DS8/src/DS8_Dijkstras.java
@@ -4,6 +4,42 @@ public class DS8_Dijkstras
     {
         // take code from DS8_BSF class breadthFirstSearch_Unweighted method
         // and then implement a way to add numbers
+        boolean[] bt = new boolean[vertices.length()];
+        DS8_Queue<char[]> queue = new DS8_Queue<>();
+        queue.add(new char[] {start});
+        bt[vertices.indexOf(start)] = true;
+
+        while (!queue.isEmpty())
+        {
+            char[] path = queue.poll();
+            char lastLetter = path[path.length-1];
+            bt[vertices.indexOf(lastLetter)] = true;
+
+            if (lastLetter == end)
+                return new String(path);
+            DS8_Queue<Character> add = new DS8_Queue<>();
+            for (int i = 0; i < edges.length; i++) {
+                if (edges[i].contains(lastLetter +""))
+                {
+                    char next;
+                    if (edges[i].startsWith(lastLetter +""))
+                        next = edges[i].charAt(1);
+                    else next = edges[i].charAt(0);
+
+                    if (!bt[vertices.indexOf(next)])
+                        add.add(next);
+                    if (!bt[vertices.indexOf(next)]){
+                        bt[vertices.indexOf(next)] = true;
+                        char[] newPath = new char [path.length+1];
+                        for (int k = 0; k < path.length; k++) {
+                            newPath[k] = path[k];
+                        }
+                        newPath[newPath.length-1] = add.poll();
+                        queue.add(newPath);
+                    }
+                }
+            }
+        }
         return -1;
     }
 }
