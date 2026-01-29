37238e4df25899d9f6a0b10b9e2e67a231537f63
KrishCore <krishvadaga4@gmail.com>
2026-01-29 16:15:52 -0600

    Update DS8_Dijkstras.java

--- DS8/src/DS8_Dijkstras.java
+++ DS8/src/DS8_Dijkstras.java
@@ -1,3 +1,4 @@
+import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -6,13 +7,10 @@ public class DS8_Dijkstras
     public static int dijkstras_Weighted(String[] edges, String vertices, char start, char end) {
         ArrayList<DS8_Weighted_Node> sorted = new ArrayList<>();
         sorted.add(new DS8_Weighted_Node(start, 0));
-        for (int i = 0; i < vertices.length(); i++) {
-            char a = vertices.charAt(i);
+        for (char a : vertices.toCharArray())
             if (a != sorted.getFirst().getLocation())
                 sorted.add(new DS8_Weighted_Node(a, Integer.MAX_VALUE));
-        }
         Collections.sort(sorted);
-
 
         while (!sorted.isEmpty())
         {
@@ -22,58 +20,97 @@ public class DS8_Dijkstras
             if (n.getLocation() == end)
                 return n.getDistance();
 
-            //arraylist to find adjacent nodes from string[] edges
             ArrayList<DS8_Weighted_Node> adj = new ArrayList<>();
-            for (String edge : edges) {
+            for (String edge : edges)
                 if (edge.charAt(0) == n.getLocation())
                     adj.add(new DS8_Weighted_Node(edge.charAt(1), Integer.parseInt(edge.substring(2))));
-            }
-            for (DS8_Weighted_Node temp: adj) {
-                int newDistance = n.getDistance() + temp.getDistance();
+            for (DS8_Weighted_Node temp: adj)
                 for (DS8_Weighted_Node node : sorted)
-                    if (node.getLocation() == temp.getLocation() && newDistance < node.getDistance())
-                        node.setDistance(newDistance);
-            }
+                    if (node.getLocation() == temp.getLocation() && n.getDistance() + temp.getDistance() < node.getDistance())
+                        node.setDistance(n.getDistance() + temp.getDistance());
             Collections.sort(sorted);
         }
         return -1;
     }
 
-    public static void quickSort(DS8_Weighted_Node[] data, int from, int to)
+    public static int dijkstras_Topographical(char[][] grid, ArrayList<DS8_TerrainCost> TerrainCosts)
     {
-        if (from >= to)
-            return;
+        ArrayList<DS8_Terrian_Node> sorted = new ArrayList<>();
+        for (int i = 0; i < grid.length; i++)
+            for (int j = 0; j < grid[0].length; j++)
+                if (grid[i][j] == 'S')
+                    sorted.addFirst(new DS8_Terrian_Node(new Point(i,j), 0));
+                else sorted.add(new DS8_Terrian_Node(new Point(i,j), Integer.MAX_VALUE));
+        Collections.sort(sorted);
 
-        int p = (from+to)/2;
-        int i=from;
-        int j=to;
+        while (!sorted.isEmpty())
+        {
+            DS8_Terrian_Node node = sorted.removeFirst();
+            int x = node.getLocation().x;
+            int y = node.getLocation().y;
 
-        while (i<=j) {
-            if (data[i].compareTo(data[p]) <= 0)
-                i++;
-            else if (data[j].compareTo(data[p]) >= 0)
-                j--;
-            else {
-                DS8_Weighted_Node temp = data[j];
-                data[j] = data[i];
-                data[i] = temp;
-                i++;
-                j--;
+            if (grid[x][y] == 'E')
+                return node.getDistance();
+            if (node.getDistance() == Integer.MAX_VALUE)
+                break;
+
+            //up
+            if (x-1 >= 0)
+            {
+                int cost = 0;
+                for (DS8_TerrainCost tc : TerrainCosts)
+                    if (tc.getType() == grid[x-1][y])
+                        cost = tc.getCost();
+                if (cost != -1)
+                    for (DS8_Terrian_Node n : sorted)
+                        if (n.getLocation().x == x-1 && n.getLocation().y == y)
+                            if (node.getDistance() + cost < n.getDistance())
+                                n.setDistance(node.getDistance() + cost);
             }
+
+            //down
+            if (x+1 < grid.length)
+            {
+                int cost = 0;
+                for (DS8_TerrainCost tc : TerrainCosts)
+                    if (tc.getType() == grid[x+1][y])
+                        cost = tc.getCost();
+                if (cost != -1)
+                    for (DS8_Terrian_Node n : sorted)
+                        if (n.getLocation().x == x+1 && n.getLocation().y == y)
+                            if (node.getDistance() + cost < n.getDistance())
+                                n.setDistance(node.getDistance() + cost);
+            }
+
+            //left
+            if (y-1 >= 0)
+            {
+                int cost = 0;
+                for (DS8_TerrainCost tc : TerrainCosts)
+                    if (tc.getType() == grid[x][y-1])
+                        cost = tc.getCost();
+                if (cost != -1)
+                    for (DS8_Terrian_Node n : sorted)
+                        if (n.getLocation().x == x && n.getLocation().y == y-1)
+                            if (node.getDistance() + cost < n.getDistance())
+                                n.setDistance(node.getDistance() + cost);
+            }
+
+            //right
+            if (y+1 < grid[0].length)
+            {
+                int cost = 0;
+                for (DS8_TerrainCost tc : TerrainCosts)
+                    if (tc.getType() == grid[x][y+1])
+                        cost = tc.getCost();
+                if (cost != -1)
+                    for (DS8_Terrian_Node n : sorted)
+                        if (n.getLocation().x == x && n.getLocation().y == y+1)
+                            if (node.getDistance() + cost < n.getDistance())
+                                n.setDistance(node.getDistance() + cost);
+            }
+            Collections.sort(sorted);
         }
-        if (p < j) {
-            DS8_Weighted_Node temp = data[p];
-            data[p] = data[j];
-            data[j] = temp;
-            p = j;
-        }
-        else if (p > i) {
-            DS8_Weighted_Node temp = data[p];
-            data[p] = data[i];
-            data[i] = temp;
-            p = i;
-        }
-        quickSort(data, from, p-1);
-        quickSort(data, p+1, to);
+        return -1;
     }
 }
