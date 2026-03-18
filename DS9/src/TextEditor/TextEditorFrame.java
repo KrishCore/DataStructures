ab95397e7677dcb1b534bb8650fc38c998610c96
KrishCore <krishvadaga4@gmail.com>
2026-03-18 10:24:23 -0500

    Update TextEditorFrame.java

--- DS9/src/TextEditor/TextEditorFrame.java
+++ DS9/src/TextEditor/TextEditorFrame.java
@@ -3,6 +3,7 @@ package TextEditor;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.awt.event.*;
 import java.io.File;
 
 public class TextEditorFrame extends JFrame
@@ -18,12 +19,14 @@ public class TextEditorFrame extends JFrame
     private JMenuItem mi_font = new JMenuItem("Font");
     private JMenuItem mi_replace = new JMenuItem("Replace");
     private JMenuItem mi_wordCount = new JMenuItem("Word Count");
-    
+
     private JPanel jp_red = new JPanel();
     private JPanel jp_green = new JPanel();
     private JPanel jp_blue = new JPanel();
 
     private JMenu m_edit = new JMenu("Edit");
+
+    private JTabbedPane tabs =  new JTabbedPane();
 
     public TextEditorFrame()
     {
@@ -36,13 +39,52 @@ public class TextEditorFrame extends JFrame
         mb_mainMenu.add(m_file);
         mb_mainMenu.add(m_edit);
 
+        // test stuff
+        {
+            jp_red.setBackground(Color.RED);
+            jp_green.setBackground(Color.GREEN);
+            jp_blue.setBackground(Color.BLUE);
+
+            this.addComponentListener(new ComponentAdapter() {
+                @Override
+                public void componentResized(ComponentEvent e) {
+                    tabs.setBounds(0,0,getWidth()-15,getHeight()-60);
+                }
+            });
+
+            tabs.setBounds(0,0,getWidth()-15,getHeight()-60);
+            add(tabs);
+        }
         //file - needs work
         {
             //create
             {
                 m_file.add(mi_create);
                 mi_create.addActionListener(e -> {
-
+                    JTextArea text = new JTextArea();
+                    JScrollPane jsp = new JScrollPane(text);
+                    jsp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+                    text.setLineWrap(true);
+                    File file = new File("Untitled.txt");
+                    if (file.exists() || tabs.indexOfTab("Untitled") != -1)
+                    {
+                        boolean isFound = false;
+                        int c = 1;
+                        while (!isFound)
+                        {
+                            file = new File("src\\TextEditor\\Saves\\Untitled" + c);
+                            if (file.exists() || tabs.indexOfTab("Untitled" + c) != -1)
+                                c++;
+                            else isFound = true;
+                        }
+                        tabs.add("Untitled" + c, jsp);
+                    }
+                    else tabs.add("Untitled", jsp);
+                    mi_saveAs.setEnabled(true);
+                    mi_save.setEnabled(true);
+                    mi_font.setEnabled(true);
+                    mi_replace.setEnabled(true);
+                    mi_wordCount.setEnabled(true);
                 });
             }
 
